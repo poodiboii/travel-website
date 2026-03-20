11760d382e7e7d04c304d9cc4bff3c9763e419af
poodiboii <pooravbatra54@gmail.com>
2026-03-20 11:50:17 +0530

    custome pakage supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "YOUR_URL";
-const supabaseKey = "YOUR_ANON_KEY";
+const supabaseUrl = "https://iozfllxuojslynduxmcc.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvemZsbHh1b2pzbHluZHV4bWNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzOTQwMzUsImV4cCI6MjA4ODk3MDAzNX0.51z_cxifdgZK57cRQ5MgePLkuZgKWAyszoDexZB4XSo";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
